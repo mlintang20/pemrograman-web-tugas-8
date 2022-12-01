aa20454b4902a30780da07741043b7caffa3021c
mlintangp <lintangpanjerino.2002@gmail.com>
2022-12-01 11:24:20 +0700

    fix: edit title

--- form_simpan.php
+++ form_simpan.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Aplikasi CRUD Plus Upload Gambar dengan PHP</title>
+  <title>Formulir Pendaftaran Siswa Baru</title>
 
   <!-- CDN Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
